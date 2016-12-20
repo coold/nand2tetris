f8bee7cd7debdeaa2a6fa0d15fe79c434316810c
Alexandr Gorbatko <rdcoold@gmail.com>
2016-12-20 19:02:50 +0500

    finished project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,97 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+    @screenValue
+    M=0
+    @screenShouldBe
+    M=-1
+    @key
+    M=0
+
+    //key check
+    (KEYCHECK)
+    @KBD
+    D=M
+    @NOTPRESSED
+    D;JEQ
+    @PRESSED
+    D;JNE
+    (PRESSED)
+    @key
+    M=-1
+    @CHECKED
+    0;JMP
+    (NOTPRESSED)
+    @key
+    M=0
+    @CHECKED
+    0;JMP
+
+    // main loop
+    (LOOP)
+    @KEYCHECK
+    0;JMP
+    (CHECKED)
+    @FILL
+    D;JMP
+
+    //fill black
+    (FILL)
+    //go back if already filled
+    @screenValue
+    D=M
+    @key
+    D=D-M
+    @LOOP
+    D;JEQ
+    // fill if not
+
+    // swap values
+    @screenValue
+    D=M
+    @temp
+    M=D
+    @screenShouldBe
+    D=M
+    @screenValue
+    M=D
+    @temp
+    D=M
+    @screenShouldBe
+    M=D
+
+
+    //take screen pointer
+    @SCREEN
+    D=A
+    @screenPointer
+    M=D
+
+    //main fill loop
+    (SCREENLOOP)
+    @screenValue
+    D=M
+    //goto pointer
+    @screenPointer
+    A=M
+    
+    M=D
+    //increment pointer
+    @screenPointer
+    M=M+1
+    D=M
+    // do fill again if not KBD register
+    @KBD
+    D=A-D
+    @SCREENLOOP
+    D;JNE
+    @LOOP
+    0;JMP
+
+
+
+
+
+
+
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,49 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    // initialization
+    @0
+    D=A
+    @R2
+    M=D
+    @acc
+    M=D
+    @R1
+    D=M
+    @count
+    M=D
+
+
+    //loop start
+    (LOOP)
+    // if second number == 0 then end loop
+    @count
+    D=M
+    @END
+    D;JEQ
+
+    // sum first number to accumulator
+    @R0
+    D=M
+    @acc
+    M=M+D
+   
+    // second--; if second != 0 jump to LOOP
+    @count
+    M=M-1
+    D=M
+    @LOOP
+    D;JNE
+
+    // ram[2] = accumulator
+    (END)
+    @acc
+    D=M
+    @R2
+    M=D
+
+    // app end
+    (APPEND)
+    @APPEND
+    0;JMP
+
